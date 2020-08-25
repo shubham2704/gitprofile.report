dd41c5aac140b5147bd47b5b3430605e54fd03f3
shubham2704 <cool.shubham2704@gmail.com>
2020-08-25 22:35:58 +0530

    added Gitigonore

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const input = document.getElementById('username');
 const name = document.querySelector('h1');
-const name2 = document.querySelector('h2');
+const bio = document.querySelector('h2');
+const image = document.querySelector('.profile');
 const submit = document.getElementById('button');
 const buttonElement = document.getElementById('btn');
 const url = "https://api.github.com/users/"
@@ -13,7 +14,9 @@ async function showResults(){
 	const data=await fetch(`${url}${input.value}`)
 	const fdata=await data.json();
     console.log(fdata)
-    name2.innerHTML = fdata.login
+    name.innerHTML = fdata.login
+    bio.innerHTML = fdata.bio;
+    image.innerHTML = `<img src = "${fdata.avatar_url}">`;
 
 }
 
